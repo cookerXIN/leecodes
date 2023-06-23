6ff98468619de2e28b75440b8c5d6ed16acf1352
‘guixin’ <1628138456@qq.com>
2023-06-23 11:40:41 +0800

    二叉树

--- src/com/liao/binaryTree/TreeNode.java
+++ src/com/liao/binaryTree/TreeNode.java
@@ -6,4 +6,14 @@ package com.liao.binaryTree;
  * @apiNote
  */
 public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+    }
 }
